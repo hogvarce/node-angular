808904648fe1e08d59c463f7b9ba32fa1d0d5edd
hogvarce <hogvarce@gmail.com>
2016-02-29 21:59:51 +0300

    fix bug

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -1,9 +1,6 @@
 angular.module('userService', [])
-
     .factory('User', function($http){
-
         var userFactory = {};
-
         userFactory.create = function(userData){
             return $http.post('/api/signup', userData);
         };
@@ -13,5 +10,4 @@ angular.module('userService', [])
         };
 
         return userFactory;
-
     });
