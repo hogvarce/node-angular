1c8e2009f6dda6f643a9503ab9339bcac2acb88b
hogvarce <hogvarce@gmail.com>
2016-03-05 00:20:04 +0300

    another try

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     "database": "mongodb://root:root@dbh35.mlab.com:27357/storyuser",
-    "port": process.env.PORT || 3000,
+    "port": process.env.PORT || 8080,
     "secretKey": "YourSecretKey"
 
 };
